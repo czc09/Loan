e7c3626fc53666f4b634990d905b00ff2849c07f
蔡志成 <caizhicheng@ofo.com>
2018-03-19 17:04:44 +0800

    第一版代码

---
+++ resources/views/home/index/four.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
+	<title></title>
+	<link href="/home/style.css" type="text/css" rel="stylesheet" />
+	<script type="text/javascript" src="/home/js/jquery-1.7.1.min.js"></script>
+	<style>
+		.file {
+			position: relative;
+			display: inline-block;
+			background: #D0EEFF;
+			border: 1px solid #99D3F5;
+			border-radius: 4px;
+			padding: 4px 12px;
+			overflow: hidden;
+			color: #1E88C7;
+			text-decoration: none;
+			text-indent: 0;
+			line-height: 20px;
+		}
+		.file input {
+			position: absolute;
+			font-size: 100px;
+			right: 0;
+			top: 0;
+			opacity: 0;
+		}
+		.file:hover {
+			background: #AADFFD;
+			border-color: #78C3F3;
+			color: #004974;
+			text-decoration: none;
+		}
+		.aixuexi img{width:100%;height:100%;}
+
+		.aixuexi{
+			width:200px;
+			height:200px;
+		}
+
+
+	</style>
+</head>
+<body>
+<div class="t-top"><!--头部-->
+	<div onMouseOut="$('#t-nav').hide();" onMouseOver="$('#t-nav').show();"style="background:#D2B48C">
+		<span>贷款平台</span>
+	</div>
+
+</div><!--头部-->
+<body style="background:#000000">
+<div class="t-main4">
+
+	<div style="text-align:center;
+            line-height: 20px;padding-top:80px;" >
+
+		 <h4 style="color:#F5F5F5 ;line-height:25px;">提交成功!</h4>
+		<h4 style="color:#F5F5F5 ;line-height:25px;">我们会第一时间审核,请您耐心等待!</h4>
+		<a href="/home/one" style="color:#00C5CD ;line-height:25px;">点击返回</a>
+		<br/><br/>
+			<h4 style="color:#76EEC6;line-height:25px;">加速处理进度请加微信:</h4>
+		<h4 style="color:#76EEC6;line-height:25px;">&nbsp;&nbsp;&nbsp;微信号:CKSCHWADJ</h4>
+		<h4 style="color:#76EEC6;line-height:25px;">微信扫描添加</h4>
+
+       <div style="padding-top:20px;"><img class="layui-upload-img " id="demo1" src="http://localhost/storage/uploads/84qbhXgVC9.jpg">
+    </div>
+
+
+	</div>
+
+
+	<script>
+
+	</script>
+</div>
+
+</body>
+<script type="text/javascript" src="/layadmin/layui/layui.js"></script>
+<script type="text/javascript" src="/layadmin/modul/home/three.js"></script>
+</html>
+
